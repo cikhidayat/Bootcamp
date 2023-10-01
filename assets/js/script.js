d993bb958f0c43decfddf40bd262c9f187ca2135
Rahmat Hidayat <rhidayat674@gmail.com>
2023-10-01 14:36:42 +0700

    day 4 - making blog

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,118 @@
+// Contact script
 function getData() {
     const getName = document.getElementById("name").value
     const getEmail = document.getElementById("email").value
     const getPhone = document.getElementById("phone").value
     const getSubject = document.getElementById("subject").value
     const getMessage = document.getElementById("message").value
-    // console.log(getName)
-    // console.log(getEmail)
-    // console.log(getPhone)
-    // console.log(getSubject)
-    // console.log(getMessage)
+
+    if(getName == ""){
+        return alert("Nama harus di isi")
+      } else if(getEmail == "") {
+        return alert("Email harus di isi") 
+      } else if(getPhone == "") {
+        return alert("Nomor telpon harus di isi") 
+      } else if(getSubject == "") {
+        return alert("subject harus di isi") 
+      } else if(getMessage == "") {
+        return alert("Message harus di isi") 
+      }
+
     const myEmail = "rhidayat674@gmail.com"
     let a = document.createElement("a")
-    a.href = `mailto:${myEmail}?subject=${getSubject}&body=Halo nama saya ${getName} bisakah anda menghubungi saya di ${getPhone} untuk membahas ${getMessage}`
+    a.href = `mailto:${myEmail}?subject=${getSubject}&body=Halo nama saya ${getName} bisakah anda menghubungi saya di nomor ${getPhone} atau email ${getEmail} untuk membahas ${getMessage}`
 
     a.click()
+};
+
+// Blog script
+const data = []
+
+function submitData(e){
+    e.preventDefault()
+
+    let projectName = document.getElementById("project-name").value
+    let startDate = document.getElementById("start-date").value
+    let endDate = document.getElementById("end-date").value
+    let description = document.getElementById("description").value
+    let technologiesHtml = document.getElementById("html").checked
+    let technologiesCss = document.getElementById("css").checked
+    let technologiesJavascript = document.getElementById("javascript").checked
+    let technologiesPhp = document.getElementById("php").checked
+    let image = document.getElementById("image-blog").files
+
+    image = URL.createObjectURL(image[0])
+
+    // Month Duration
+    let startYear = Number(startDate.slice(0, 4))
+    let endYear = Number(endDate.slice(0, 4))
+    let startMonth = Number(startDate.slice(5, 7))
+    let endMonth = Number(endDate.slice(5, 7))
+
+    let yearMonthCv = (endYear-startYear) * 12
+    let monthDuration = (endMonth - startMonth) + yearMonthCv
+
+    // Technologies
+    if (technologiesHtml === true) {
+        technologiesHtml = `<i class="fa-brands fa-html5"></i>`
+    } else {
+        technologiesHtml = ""
+    };
+    if (technologiesCss === true){
+        technologiesCss = `<i class="fa-brands fa-css3-alt"></i>`
+    } else {
+        technologiesCss = ""
+    };
+    if (technologiesJavascript === true){
+        technologiesJavascript = `<i class="fa-brands fa-js"></i>`
+    } else {
+        technologiesJavascript = ""
+    };
+    if (technologiesPhp === true){
+        technologiesPhp = `<i class="fa-brands fa-php"></i>`
+    } else {
+        technologiesPhp = ""
+    };
+
+    const projectData = {
+        projectName,
+        startDate,
+        endDate,
+        monthDuration,
+        description,
+        technologiesHtml,
+        technologiesCss,
+        technologiesJavascript,
+        technologiesPhp,
+        image
+    }
+
+    data.push(projectData)
+    renderBlog()
+}
+
+function renderBlog(){
+    console.log(data)
+    document.getElementById("project-blog").innerHTML = ""
+
+    for(let i = 0; i<data.length; i++){
+        document.getElementById("project-blog").innerHTML +=
+        `<div class="project-post">
+            <img src="${data[i].image}" alt=""/>
+            <a href="blog-page.html" target="_blank"><h3 class="project-title">${data[i].projectName}</h3></a>
+            <label for="project-duration">Duration: </label>
+            <p class="project-duration"> ${data[i].monthDuration} Month</p>
+            <p class="project-desc">${data[i].description}</p>
+            <p class="project-tech">
+                ${data[i].technologiesHtml}
+                ${data[i].technologiesCss}
+                ${data[i].technologiesJavascript}
+                ${data[i].technologiesPhp}
+            </p>
+            <div class="action-btn">
+                <input type="button" value="Edit">
+                <input type="button" value="Delete">
+            </div>
+        </div>`
+    }
 }
