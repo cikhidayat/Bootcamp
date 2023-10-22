cf1ff14bba375c59b7e34ebe7dd6e584c7bd0671
Rahmat Hidayat <rhidayat674@gmail.com>
2023-10-23 01:03:13 +0700

    day 14 - authentication

---
+++ src/migrations/20231019174643-create-myblog.js
@@ -0,0 +1,65 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('myblogs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      startDate: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      endDate: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      duration: {
+        allowNull: false,
+        type: Sequelize.BIGINT
+      },
+      html: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      css: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      javascript: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      php: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('myblogs');
+  }
+};
---
+++ src/models/myblog.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class myblog extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  myblog.init({
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    startDate: DataTypes.STRING,
+    endDate: DataTypes.STRING,
+    duration: DataTypes.NUMBER,
+    html: DataTypes.BOOLEAN,
+    css: DataTypes.BOOLEAN,
+    javascript: DataTypes.BOOLEAN,
+    php: DataTypes.BOOLEAN,
+    image: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'myblog',
+    timestamps: true,
+    createdAt: true,
+    updatedAt: true
+  });
+  return myblog;
+};
